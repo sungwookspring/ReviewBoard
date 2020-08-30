426c1771b4c1cec4ca834cd9e8687d67292b12b6
choisungwook <kgg1959@naver.com>
2020-08-30 22:00:31 +0900

    Add post saveRestController 테스트

---
+++ src/test/java/com/reviewboard/sungwook/Web/PostApiControllerTest.java
@@ -0,0 +1,56 @@
+package com.reviewboard.sungwook.Web;
+
+import com.reviewboard.sungwook.Domain.PostRepository;
+import com.reviewboard.sungwook.Domain.Posts;
+import com.reviewboard.sungwook.Web.Dto.PostSaveRequestDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void 게시글등록(){
+        //given
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        String title = "title";
+        String content = "content";
+        PostSaveRequestDto requestDto = PostSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("aaa")
+                .build();
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
