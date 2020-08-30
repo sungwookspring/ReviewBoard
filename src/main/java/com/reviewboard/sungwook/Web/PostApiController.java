506139461a2744f972cfd6e391a9fefb1d350a2d
choisungwook <kgg1959@naver.com>
2020-08-30 21:56:26 +0900

    Revert "Change postRestAPIController method: Get -> Post"
    This reverts commit 08b4c55e3926d19c62d51bd3267dfe51cd3db168.

--- src/main/java/com/reviewboard/sungwook/Web/PostApiController.java
+++ src/main/java/com/reviewboard/sungwook/Web/PostApiController.java
@@ -3,7 +3,8 @@ package com.reviewboard.sungwook.Web;
 import com.reviewboard.sungwook.Service.PostService;
 import com.reviewboard.sungwook.Web.Dto.PostSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostApiController {
     private final PostService postService;
 
-    @PostMapping("/api/v1/posts")
+    @GetMapping("/api/v1/posts")
     public long save(@RequestBody PostSaveRequestDto requestDto){
         return postService.save(requestDto);
     }
